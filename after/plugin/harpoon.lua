6fb9b3e0557e6592d57fa0e263b6a374cd2ea0dc
Paul Louvel <plouvel@student.42.fr>
2023-02-15 10:15:38 +0100

    added harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,6 +3,8 @@ local ui = require('harpoon.ui')
 
 vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu)
 vim.keymap.set("n", "<leader>ha", mark.add_file)
+vim.keymap.set("n", "<leader>hn", ui.nav_next)
+vim.keymap.set("n", "<leader>hp", ui.nav_prev)
 
 vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
